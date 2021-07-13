31c5b6db74083dbae7f8d6b7f3e5dfd5e3526f9e
godzillalogan <kenlogan0117@gmail.com>
2021-07-13 11:49:48 +0800

    2021.7.13,外, 重構路由器 home.js index.js

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const methodOverride = require('method-override')
 
 const Restaurant = require('./models/restaurant')
 
+const routes = require('./routes')  // 引用路由器
+
 // const restaurant = require('./models/restaurant')
 
 
@@ -33,6 +35,7 @@ db.once('open', () => {
 // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(routes)  // 將 request 導入路由器
 // setting template engine, extname: '.hbs'，是指定副檔名為 .hbs，有了這行以後，我們才能把預設的長檔名改寫成短檔名
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
---
+++ routes/modules/home.js
@@ -0,0 +1,19 @@
+// 引用 Express 與 Express 路由器
+const express = require('express')
+
+const router = express.Router()
+
+const Restaurant = require('../../models/restaurant')
+
+
+
+// routes setting
+router.get('/', (req, res) => {
+
+  Restaurant.find()
+    .lean()
+    .then(restaurants => res.render('index', {restaurants}))
+    .catch(error => console.log(error))
+})
+
+module.exports = router
