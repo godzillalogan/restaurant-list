42d7342227e83e4efa1f9443a683e8fbdab1ea54
godzillalogan <63105483+godzillalogan@users.noreply.github.com>
2021-08-07 14:29:17 +0800

    U15 設定 Users Router

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
+const users = require('./modules/users')
 
 // 將網址結構符合 / 字串的 request 導向 home 模組 
 router.use('/', home)
 router.use('/', restaurants)
+router.use('/users', users)
 
 // 準備引入路由模組
 // 匯出路由器
