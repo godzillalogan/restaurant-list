be58a923236b7deb4077c3449d4481f647d19fe1
godzillalogan <63105483+godzillalogan@users.noreply.github.com>
2021-08-07 21:46:00 +0800

    U25 驗證登入狀態:失敗情況

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,16 @@ const router = express.Router()
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
 const users = require('./modules/users')
+const { authenticator } = require('../middleware/auth')
 
-// 將網址結構符合 / 字串的 request 導向 home 模組 
-router.use('/', home)
-router.use('/', restaurants)
+
+
+
 router.use('/users', users)
 
-// 準備引入路由模組
 // 匯出路由器
 module.exports = router
+
+router.use('/', authenticator, restaurants)
+// 將網址結構符合 / 字串的 request 導向 home 模組 
+router.use('/', authenticator, home)
